92a38f886e50b9ab2534ab1a3c0fc88d38975194
Greg Poulos <greg@rapleaf.com>
2011-03-22 10:06:41 -0700

    update user agent string

--- lib/rapleaf_api.rb
+++ lib/rapleaf_api.rb
@@ -24,7 +24,7 @@ include ERB::Util
 module RapleafApi
   HOST = "personalize.rapleaf.com"
   PORT = 443
-  HEADERS = {'User-Agent' => 'RapleafApi/Ruby/1.2.0'}
+  HEADERS = {'User-Agent' => 'RapleafApi/Ruby/1.2.1'}
 
   class Api
     def initialize(api_key, options = {})
